bbe68ac9914efc59d1a31692a030849e6fe2d6f6
SvineruS <37367870+SvineruS@users.noreply.github.com>
2020-01-30 22:09:55 +0200

    small naming changes

--- main.py
+++ main.py
@@ -30,17 +30,20 @@ def get_id_from_watch_url(url):
 
 
 def get_download_urls_by_id(id_):
-    resp = requests.get(f"https://www.youtube.com/get_video_info?video_id={id_}").text
-    resp = parse.unquote(resp)
-    resp = resp.split("player_response=")[1].split("&")[0]
-    resp = json.loads(resp)
-    if resp['playabilityStatus']['status'] == 'UNPLAYABLE':
-        raise Exception(resp['playabilityStatus']['reason'])
+    resp = requests.get(f"https://www.youtube.com/get_video_info?video_id={id_}")
+    video_info = parse.unquote(resp.text)
+    player_response_string = video_info.split("player_response=")[1].split("&")[0]
+    player_response_json = json.loads(player_response_string)
+    if player_response_json['playabilityStatus']['status'] == 'UNPLAYABLE':
+        raise Exception(player_response_json['playabilityStatus']['reason'])
 
-    resp = resp['streamingData']['formats']
-    resp = sorted(resp, key=lambda i: i['width'] * i['height'], reverse=True)
-    resp = {f"{i['width']}x{i['height']}": i['url'] for i in resp}
-    return resp
+    streaming_data = player_response_json['streamingData']
+    # look at streaming_data['adaptiveFormats']. it contains more formats, for example mp3
+
+    formats = streaming_data['formats']
+    formats_sorted_by_quality = sorted(formats, key=lambda i: i['width'] * i['height'], reverse=True)
+    formats_dict_by_quality = {f"{i['width']}x{i['height']}": i['url'] for i in formats_sorted_by_quality}
+    return formats_dict_by_quality
 
 
 if __name__ == "__main__":
